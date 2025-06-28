441d0afadd243ebc2f6ae6e0e8b15f6b41873180
lomanski <jmlomanski@gmail.com>
2025-06-28 11:28:55 +0200

    initail commit

---
+++ 01_Basics/question.h
@@ -0,0 +1,8 @@
+//
+// Created by lomanski on 28.06.2025.
+//
+
+#ifndef QUESTION_H
+#define QUESTION_H
+
+#endif //QUESTION_H
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cpp_object
+
+In this repo I'm doing some basics of object C++
