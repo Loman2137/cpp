4f4414062b672c55bb493b6dfb473846eace0b18
lomanski <jmlomanski@gmail.com>
2025-07-02 22:51:13 +0200

    zad 4.2

--- Matury/2022/main.cpp
+++ Matury/2022/main.cpp
@@ -1,46 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <set>
 #include <vector>
 
 using namespace std;
 
 vector<int> kontener;
-int liczba;
+int liczba{};
 
 int main() {
+
     int z4_1ile{}, z4_1num{};
     int z4_2roz{}, z4_2pop_roz{}, z4_2num{};
+    set<int>pierwsze_pop; set<int>pierwsze; int z4_2liczpier;
 
     fstream wczytanie("Dane_2205/przyklad.txt", ios::in); //ios::in jest domyślny
     fstream zapisanie("Dane_2205/wyniki4.txt", ios::out); //ios::out ma domyślnie nadpisywanie wartości
     cout<<"kompilacja"<<endl;
 
-    // while (wczytanie >> liczba) {
-    //     //kontener.push_back(liczba);
-    //
-    //     //4.1
-    //     string h = to_string(liczba);
-    //     if (h[0]==h[h.size()-1]) {
-    //         z4_1ile++;
-    //         if (z4_1num == 0)
-    //             z4_1num=liczba;
-    //     }
-    // }
-
     while (wczytanie >> liczba) {
-        //4.2
-        z4_2roz=0;
-        for (int i = 2; i < liczba;) {
+        //kontener.push_back(liczba);
 
-            if (liczba%i == 0) {
-                cout<<"liczba równa się: "<<liczba<<", a dzielnik równa się: "<<i<<endl;
-                liczba = liczba / i;
+        //4.1
+         string h = to_string(liczba);
+         if (h[0]==h[h.size()-1]) {
+             z4_1ile++;
+             if (z4_1num == 0)
+                 z4_1num=liczba;
+         }
+
+        //4.2
+        int zamiennik = liczba;
+        z4_2roz=0;
+        pierwsze.clear();
+        for (int i = 2; i <= zamiennik;) {
+            if (zamiennik%i == 0) {
+                zamiennik = zamiennik / i;
                 z4_2roz++;
+                pierwsze.insert(i);
+                if (z4_2roz>z4_2pop_roz) {
+                    z4_2pop_roz=z4_2roz;
+                    z4_2num = liczba;
+                }
+                if (pierwsze.size()>pierwsze_pop.size()) {
+                    pierwsze_pop=pierwsze;
+                    z4_2liczpier = liczba;
+                }
             }
-            if (z4_2roz>z4_2pop_roz) {
-                z4_2pop_roz=z4_2roz;
-                z4_1num=liczba;
-            }
+            else
+                i++;
         }
     }
 
@@ -49,7 +57,7 @@ int main() {
 
 
     zapisanie << z4_1ile<<" "<<z4_1num<<endl;
-    zapisanie << z4_2pop_roz << " " << z4_2num <<endl;
+    zapisanie << z4_2num << " " << z4_2pop_roz<<" "<<z4_2liczpier<<" "<< pierwsze_pop.size()<<endl;
     wczytanie.close();
     zapisanie.close();
 }
